65ba60b8f526607de2058ffcdf37639d50dae011
i.bobrov <i.bobrov@itfbgroup.ru>
2025-02-02 07:30:29 +0300

    первая корутина

--- src/main/kotlin/ru/bobrov/coroutines/CoroutinesApplication.kt
+++ src/main/kotlin/ru/bobrov/coroutines/CoroutinesApplication.kt
@@ -1,10 +1,28 @@
 package ru.bobrov.coroutines
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CoroutinesApplication
+class CoroutinesApplication : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        runBlocking {
+            launch { simulateLoading() }
+        }
+    }
+
+    suspend fun simulateLoading() {
+        for (i in 1..100) {
+            println("Loading $i%")
+            delay(100)
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CoroutinesApplication>(*args)
